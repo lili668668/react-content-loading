279dd68be7b005163a9777fac16ae0612a9bef3f
Tzu-Yin Hong <lili668668@gmail.com>
2019-02-19 10:36:27 +0800

    Add css linear gradient string builder

---
+++ src/utils/linearGradient.js
@@ -0,0 +1,30 @@
+class linearGradient {
+  constructor () {
+    this.definition = ''
+    this.stops = []
+  }
+
+  angle(angle) {
+    if (typeof angle === 'number') this.definition = `${angle}deg`
+    else if (typeof angle === 'string') this.definition = angle
+    else throw new TypeError('Please input number or string for angle param')
+    return this
+  }
+
+  to(...sides) {
+    this.definition = `to ${sides.join(' ')}`
+    return this
+  }
+
+  appendColorStop(color, length) {
+    if (length !== undefined) this.stops.push(`${color} ${length}`)
+    else this.stops.push(color)
+    return this
+  }
+
+  toString() {
+    return `linear-gradient(${this.definition}, ${this.stops.join(', ')})`
+  }
+}
+
+export default linearGradient
