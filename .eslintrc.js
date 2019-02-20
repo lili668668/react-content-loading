5d6e32e6f32c075fc1f5862f1bfde08c6c6448e6
Tzu-Yin Hong <lili668668@gmail.com>
2019-02-20 18:00:08 +0800

    Add jest eslint plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,8 @@
 module.exports = {
   'env': {
     'browser': true,
-    'es6': true
+    'es6': true,
+    'jest/globals': true
   },
   'extends': [
     'eslint:recommended',
@@ -24,7 +25,8 @@ module.exports = {
     'sourceType': 'module'
   },
   'plugins': [
-    'react'
+    'react',
+    'jest'
   ],
   'rules': {
     'indent': [
