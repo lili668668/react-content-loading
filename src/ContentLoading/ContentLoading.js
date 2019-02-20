783c8a2142c02d9e8f6b98f4ce124a3bb987d072
Tzu-Yin Hong <lili668668@gmail.com>
2019-02-20 18:57:25 +0800

    Fix name error

--- src/ContentLoading/ContentLoading.js
+++ src/ContentLoading/ContentLoading.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import Color from 'color'
-import LinearGradient from '../utils/linearGradient'
+import LinearGradient from '../utils/LinearGradient'
 
 export const getHeight = props => props.height
 export const getWidth = props => props.width
